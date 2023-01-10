82dca061328e56c7b77c949f5f253158a3b46dfc
DXVIL デビル <musicbyblakk@gmail.com>
2023-01-10 16:07:58 +0100

    Better table of users in admin panel (0x23011016)

--- functions/query.php
+++ functions/query.php
@@ -1,14 +1,21 @@
 <?php
     include "./connectSQL.php";
-    $username = $_POST["username"];
     $fullname = $_POST["fullname"];
+    $username = $_POST["username"];
     $email = $_POST["email"];
     $role = $_POST["role"];
     $password = $username."!";
     $password = hash("sha512", $password);
-    if (empty($username)) { header("Location: ../index.php?request=admin&error=query_failed"); }
-    $query = 'INSERT INTO users VALUES("'.$username.'","'.$password.'","'.$role.'","'.$fullname.'","'.$email.'") ON DUPLICATE KEY UPDATE username = "'.$username.'", role = "'.$role.'", email = "'.$email.'", fullname = "'.$fullname.'";';
-    if($con -> query($query)){
+if (isset($_POST["firstTimeUser"]))
+    $firstTimeUser = 1;
+else
+    $firstTimeUser = 0;
+    if(!mysqli_query($con,'INSERT INTO users VALUES("'.$username.'","'.$password.'","'.$role.'","'.$fullname.'","'.$email.'", "'.$firstTimeUser.'") ON DUPLICATE KEY UPDATE username = "'.$username.'", role = "'.$role.'", email = "'.$email.'", fullname = "'.$fullname.'";')){
+        echo mysqli_error($con);
+    } else header("Location: ../index.php?request=admin");
+
+    /*if($con -> query($query)){
         header("Location: ../index.php?request=admin");
-    } else header("Location: ../index.php?request=admin&error=query_failed");
+} else
+    mysqli_error($con); //header("Location: ../index.php?request=admin&error=query_failed");*/
 ?>
