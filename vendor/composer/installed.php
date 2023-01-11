b13aafed22ffc28a431844c982c4f016fd93fc64
DXVIL デビル <musicbyblakk@gmail.com>
2023-01-11 13:43:25 +0100

    I hate this!

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'qbit/qbit',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '302371ccd1dc7da8d428f7c66f2284391745059c',
+        'type' => 'application',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'qbit/qbit' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '302371ccd1dc7da8d428f7c66f2284391745059c',
+            'type' => 'application',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
