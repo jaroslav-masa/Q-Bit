7830733c806d4bfce389892c1db26af6a3ceff2e
DXVIL デビル <musicbyblakk@gmail.com>
2023-01-11 15:47:08 +0100

    Increased Security and Rewritten MySQL operations from scratch

--- functions/login.php
+++ functions/login.php
@@ -4,18 +4,13 @@
     $username = isset($_POST['username']) ? $_POST['username'] : '';  
     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-    $username = stripcslashes($username);
-    $password = stripcslashes($password);
-    $username = mysqli_real_escape_string($con, $username);
-    $password = mysqli_real_escape_string($con, $password);
     $password = hash('sha512',$password);
-    echo $password;
-    $sql = "select * from users where username = '$username' and password = '$password'";
-    $result = mysqli_query($con, $sql);
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    $count = mysqli_num_rows($result);
-    
-    mysqli_close($con);
+    $stmt = $con->prepare("SELECT * FROM users WHERE username = :username AND password = :password;");
+    $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+    $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $count = $stmt->rowCount();
     if ($count == 1) {
         $_SESSION["firstTimeUser"] = $row["firstTimeUser"];
         $_SESSION["loggedIn"] = true;
